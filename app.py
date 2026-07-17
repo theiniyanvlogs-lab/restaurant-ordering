eef873a6a78dc62fb0cf799e08fc9551932182a6
Iniyan <the.iniyan.vlogs@gmail.com>
2026-07-17 06:13:55 +0000

    Updated app.py for Render deployment

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import (
     Flask,
     render_template,
@@ -829,13 +831,8 @@ def internal_server_error(error):
 # ==========================================================
 
 if __name__ == "__main__":
-
     app.run(
-
         host="0.0.0.0",
-
-        port=5000,
-
-        debug=True
-
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
     )
